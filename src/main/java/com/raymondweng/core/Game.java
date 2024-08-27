f0ca989acd46abe0f02ff0f611955f9096aa3f71
Raymond <yuhongweng0501@gmail.com>
2024-08-27 20:26:44 +0800

    close connection

--- src/main/java/com/raymondweng/core/Game.java
+++ src/main/java/com/raymondweng/core/Game.java
@@ -75,6 +75,7 @@ public class Game {
                         }
                     }
                 }
+                connection.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
