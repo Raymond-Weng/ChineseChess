200de923075e8a11a92349ad45c3aaceb640f125
Raymond <yuhongweng0501@gmail.com>
2024-09-11 21:02:05 +0800

    updates...
    - take the feature "get point" out
    - receive "%invites"

--- src/main/java/com/raymondweng/listeners/EventListener.java
+++ src/main/java/com/raymondweng/listeners/EventListener.java
@@ -1,6 +1,7 @@
 package com.raymondweng.listeners;
 
 
+import com.raymondweng.Main;
 import com.raymondweng.core.Game;
 import com.raymondweng.core.Invite;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -15,7 +16,6 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
     public boolean registered(String id) {
@@ -37,7 +37,7 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
         return registered;
     }
 
-    public boolean playing(String id){
+    public boolean playing(String id) {
         boolean playing = true;
         try {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:./database/data.db");
@@ -52,6 +52,23 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
             throw new RuntimeException(e);
         }
         return playing;
+    }
+
+    public int point(String id) {
+        int point = 0;
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:./database/data.db");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT POINT FROM PLAYER WHERE DISCORD_ID = " + id);
+            resultSet.next();
+            point = resultSet.getInt("POINT");
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return point;
     }
 
     @Override
@@ -124,29 +141,16 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
                                 } else if (playing(message.getContentRaw().split(" ")[1].substring(2, message.getContentRaw().split(" ")[1].length() - 1))) {
                                     message.reply("被邀請的人正在另一個對局，請等待對局結束後再邀請").queue();
                                 } else {
-                                    int point = 0;
-                                    try {
-                                        Connection connection = DriverManager.getConnection("jdbc:sqlite:./database/data.db");
-                                        Statement statement = connection.createStatement();
-                                        ResultSet resultSet = statement.executeQuery("SELECT POINT FROM PLAYER WHERE DISCORD_ID = " + message.getMember().getUser().getId());
-                                        resultSet.next();
-                                        point = resultSet.getInt("POINT");
-                                        resultSet.close();
-                                        statement.close();
-                                        connection.close();
-                                    } catch (SQLException e) {
-                                        throw new RuntimeException(e);
-                                    }
                                     Invite inv = Invite.createInvite(message.getMember().getId(), message.getContentRaw().split(" ")[1].substring(2, message.getContentRaw().split(" ")[1].length() - 1));
                                     if (inv == null) {
                                         message.reply("你已經發送邀請了，請等待回復或是刪除上一個邀請")
                                                 .addEmbeds(new EmbedBuilder()
-                                                        .addField("目前正在邀請...", "<@" + Invite.getInviteByInviter(message.getMember().getId()).invitee + ">", true)
+                                                        .addField("目前正在邀請...", "<@" + Invite.getInviteByInviter(message.getMember().getId()).invitee + ">", false)
                                                         .build())
                                                 .queue();
                                     } else {
                                         message.reply("邀請成功").queue();
-                                        message.getChannel().sendMessage("<@" + inv.invitee + "> 您已收到來自 <@" + message.getMember().getId() + "> (" + point + ")的邀請，請使用`%accept <邀請者>`接受挑戰，或是使用`%reject <邀請者>`來拒絕").queue();
+                                        message.getChannel().sendMessage("<@" + inv.invitee + "> 您已收到來自 <@" + message.getMember().getId() + "> (" + point(message.getMember().getId()) + ")的邀請，請使用`%accept <邀請者>`接受挑戰，或是使用`%reject <邀請者>`來拒絕").queue();
                                     }
                                 }
                             }
@@ -163,6 +167,18 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
                         message.reply("邀請已取消").queue();
                     }
                     break;
+                case "%invites":
+                    if (Invite.getInviteByInvitee(message.getMember().getId()) == null) {
+                        message.reply("目前你還沒有收到邀請").queue();
+                        break;
+                    }
+                    EmbedBuilder embedBuilder = new EmbedBuilder();
+                    embedBuilder.setTitle("以下是所有傳送給你的邀請");
+                    Invite.getInviteByInvitee(message.getMember().getId()).keySet().forEach(key -> {
+                        Main.main.jda.retrieveUserById(key).queue(member -> embedBuilder.addField(member.getName(), "(" + point(key) + ")", false));
+                    });
+                    message.replyEmbeds(embedBuilder.build()).queue();
+                    break;
                 case "%accept":
                 case "%reject":
                     if (message.getContentRaw().split(" ").length >= 2 && message.getContentRaw().split(" ")[1].matches("<\\d*>")) {
@@ -176,7 +192,7 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
                             }
-                        }else {
+                        } else {
                             Invite.getInviteByInvitee(message.getMember().getId()).get(message.getContentRaw().split(" ")[1].substring(2, message.getContentRaw().split(" ")[1].length() - 1)).remove();
                             message.reply("已拒絕邀請").queue();
                         }
@@ -198,7 +214,7 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
             if (((GuildVoiceUpdateEvent) genericEvent).getChannelJoined() != null) {
                 if (((GuildVoiceUpdateEvent) genericEvent).getChannelJoined().getId().equals("1270560414719279236")) {
                     genericEvent.getJDA()
-                                    .getCategoryById("1270560414274687009")
+                            .getCategoryById("1270560414274687009")
                             .createVoiceChannel(((GuildVoiceUpdateEvent) genericEvent).getMember().getUser().getEffectiveName() + "的語音頻道")
                             .queue(channel -> {
                                 ((GuildVoiceUpdateEvent) genericEvent).getGuild().moveVoiceMember(((GuildVoiceUpdateEvent) genericEvent).getMember(), channel).queue();
